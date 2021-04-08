d8d327694583ddb3aa07e4f9fe88eb42ae79f99b
henrylong <henrylong719@gmail.com>
2021-04-09 09:01:07 +0930

    finish create booking component

--- Booking/src/app/places/places.service.ts
+++ Booking/src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City',
       'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
-      149.9
+      149.9,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       "L'Amour Toujours",
       'A romantic place in Paris!',
       'https://cdn.shopify.com/s/files/1/0008/0802/products/P14-1198-Edit-2_1024x1024.jpg?v=1408738458',
-      189.9
+      189.9,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://s8.favim.com/orig/72/dracula-castle-fog-foggy-Favim.com-717423.jpg',
-      89.9
+      89.9,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
   ];
 
